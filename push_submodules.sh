abddf5ece8535c011d35be594ded958a0b4c7362
nomuraya <shimajima-eiji@live.jp>
2018-10-25 13:39:20 +0900

    [push] by push_submodules.sh

--- push_submodules.sh
+++ push_submodules.sh
@@ -2,5 +2,5 @@ git submodule foreach git add -A
 git submodule foreach git commit -m "${push} by $0 from githubpage repository"
 git add -A
 git commit -m "[push] by $0"
-#git push --recurse-submodules=on-demand
+git push --recurse-submodules=on-demand
 
