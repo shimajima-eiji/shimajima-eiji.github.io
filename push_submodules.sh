5338cf85397c88e7ade41f02b0bcdac12a6354a3
nomuraya <shimajima-eiji@live.jp>
2018-10-25 13:59:25 +0900

    [push] by push_submodules.sh

--- push_submodules.sh
+++ push_submodules.sh
@@ -1,6 +1,6 @@
 git submodule foreach git add -A
 git submodule foreach git commit -m "${push} by $0 from githubpage repository"
+git push --recurse-submodules=on-demand origin master
 git add -A
 git commit -m "[push] by $0"
-git push --recurse-submodules=on-demand origin master
 
