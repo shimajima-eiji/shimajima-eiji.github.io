44adc6d0360c8b3c0b6253ba68bfb1a5a70eab7b
shimajima-eiji <shimajima-eiji@live.jp>
2018-11-21 08:06:47 +0900

    [push] by /opt/src/gh-pages/utils/ope/github/push_submodules.sh

--- gitremote.bash
+++ gitremote.bash
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 getremote() {
   cd $1
-  local_tmp=tmp
+  local_tmp=$2
+
   if [ ! "$(git branch | grep ${local_tmp})" == '' ]; then
     git branch -d ${local_tmp}
   fi
@@ -9,22 +10,13 @@ getremote() {
     git remote set-url origin $(git remote get-url --push origin | sed 's#https://github.com/#git@github.com:#').git
     git remote get-url --push origin
   fi
-
-  git pull origin ${master}
-  git add -A
-  git commit -m "[push] by $0"
-  git branch ${local_tmp}
-  git checkout ${master}
-  git merge ${local_tmp}
-  git branch -d ${local_tmp}
-  git push --recurse-submodules=on-demand origin ${master}
-
   cd ..
 }
 
+local_tmp="${1:-tmp}"
 current=$(dirname ${0})
 loop=$(find ${current} -maxdepth 1 -mindepth 1 -type d | grep -v .git$)
 for dir in ${loop}
 do
-  getremote ${dir}
+  getremote ${dir} ${local_tmp} ${remote_push} ${commit_message}
 done
